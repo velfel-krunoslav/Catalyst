9c43e910fbac63d4aa7a1116716ccdb13f4edac8
Krunoslav Velfel <kvelfel@gmail.com>
2021-03-29 16:34:50 +0200

    Deleted residual files.

--- .Trash-1000/files/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:frontend_mobile/pages/categories_page.dart';
-import 'package:frontend_mobile/pages/home_page.dart';
-
-void main() {
-  runApp(MaterialApp(home: HomePage()));
-}
